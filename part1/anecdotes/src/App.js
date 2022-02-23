4e5110f924ceb36b55a9dbc2c6ea0d269bbb7fca
Johanna Lehto <johannaeveliinalehto@gmail.com>
2022-02-23 14:25:59 +0200

    Anecdotes done

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,6 +1,45 @@
 import React, { useState } from "react"
 
 
+const DailyAnecdote = ({ anecdote, points }) => {
+    return (
+        <>
+            <h1>Anecdote of the day</h1>
+            <h4>{anecdote}</h4>
+            <p>has {points} votes</p>
+        </>
+    )
+}
+
+
+const Button = ({ handleEvent, text }) => {
+    return (
+        <>
+            <button onClick={handleEvent}>{text}</button>
+        </>
+    )
+}
+
+const MostPopularAnecdote = ({ anecdote, points }) => {
+
+    if (points > 0) {
+        return (
+            <>
+                <h1>Most popular anecdote</h1>
+                <h5>{anecdote}</h5>
+                <p>has {points} votes</p>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <h1>Most popular anecdote</h1>
+                <p>No votes yet</p>
+            </>
+        )
+    }
+}
+
 const App = () => {
     const anecdotes = [
         'If it hurts, do it more often',
@@ -12,45 +51,43 @@ const App = () => {
         'Perfection (in design) is achieved not when there is nothing more to add, but rather when there is nothing more to take away',
         'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
         'Program testing can be used to show the presence of bugs, but never to show their absence!'
-      ]
+    ]
 
+    const [selected, setSelected] = useState(0)
+    const [points, setPoints] = useState(new Uint8Array(9))
+    const [mostVoted, setMostVoted] = useState(0)
 
-      const [selected, setSelected] = useState(0)
-      const [points, setPoints] = useState(new Uint8Array(9))
-      const [mostVoted, setMostVoted] = useState(0)
-
-     
-
-      const handleNext = () => {
+    const handleNext = () => {
         setSelected(Math.floor(Math.random() * anecdotes.length))
-      }
+    }
 
-      const setMostPopular = () => {
-        const maxVotes = Math.max( ...points)
-        setMostVoted(points.indexOf(maxVotes))
-      }
+    const setMostPopular = () => {
+        const mostPoints = Math.max(...points)
+        const mostPointsIndex = points.indexOf(mostPoints)
+        setMostVoted(mostPointsIndex)
+    }
 
-      const handleVote = () => {
+    const handleVote = () => {
         const copy = [...points]
         copy[selected] += 1
         setPoints(copy)
         setMostPopular()
-      }
+    }
 
-  
-
-    return( 
+    return (
         <>
-        <h1>Anecdote of the day</h1>
-        <h2>{anecdotes[selected]}</h2>
-        <p>has {points[selected]} votes</p>
-        <button onClick={handleVote}>Vote</button>
-        <button onClick={handleNext}>Next</button>
-        <h1>Most popular anecdote</h1>
-        <h2>{anecdotes[mostVoted]}</h2>
-        <p>has {points[mostVoted]} votes</p>
-
-
+            <DailyAnecdote
+                anecdote={anecdotes[selected]}
+                points={points[selected]} />
+            <Button
+                handleEvent={handleVote}
+                text={"Vote"} />
+            <Button
+                handleEvent={handleNext}
+                text={"Next"} />
+            <MostPopularAnecdote
+                anecdote={anecdotes[mostVoted]}
+                points={points[mostVoted]} />
         </>
     )
 }
